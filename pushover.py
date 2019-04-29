c4e614a81d1d0077f14c4082f21402aca396b859
virus <svirus@gmail.com>
2019-04-29 19:12:38 +0200

    add emergency support

--- pushover.py
+++ pushover.py
@@ -8,7 +8,7 @@ parser.add_argument('appkey', type=str, help='Application Token/Key')
 parser.add_argument('userkey', type=str, help='User Key')
 parser.add_argument('subject', type=str, help='Subject')
 parser.add_argument('message', help='Message')
-parser.add_argument('--emergency', metavar=('retry', 'expire'), help="set emergency mode with retry and expire time in seconds", default=False, required=False, nargs=2, type=int)
+parser.add_argument('--emergency', metavar=('retry', 'expire'), help="Set emergency mode with retry and expire time in seconds", default=False, required=False, nargs=2, type=int)
 
 
 
@@ -21,6 +21,8 @@ emergency = args.emergency
 
 prio = 0
 s = None
+retry = 0
+expire = 0
 
 for line in message.splitlines():
     x = re.findall("^Severity: ", line)
@@ -49,6 +51,11 @@ x = re.findall("^Resolved: ", subject)
 if(x):
     prio = -1
 
+if (emergency != False):
+    retry = emergency[0]
+    expire = emergency[1]
+
+
 
 conn = http.client.HTTPSConnection("api.pushover.net:443")
 conn.request("POST", "/1/messages.json",
@@ -59,8 +66,12 @@ conn.request("POST", "/1/messages.json",
     "title": subject,
     "priority": prio,
     "html": 1,
+    "retry": retry,
+    "expire": expire,
   }), { "Content-type": "application/x-www-form-urlencoded" })
-print(conn.getresponse())
+
+
+conn.getresponse()
 #    "priority": 2,
 #    "retry": 10,
 #    "expire": 60,
